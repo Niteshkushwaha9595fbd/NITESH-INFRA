66d30d4d439b218a57ad120ef8a6f3301398dd94
Niteshkushwaha9595fbd <niteshkushwaha9595fbd@gmail.com>
2025-12-29 01:23:26 +0530

    Update Provider.tf

--- env/dev/Provider.tf
+++ env/dev/Provider.tf
@@ -7,11 +7,7 @@ terraform {
   }
   backend "azurerm" {
     
-    resource_group_name              = "niteshcontainer"          # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
-    storage_account_name             = "stgnitesh2"                              # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
-    container_name                   = "niteshcontainer"                               # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                              = "tf.tfstate"                # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
-  }
+
 }
 
   
@@ -19,6 +15,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-  subscription_id = "49c47d3a-ee46-467f-b8ac-06b906f03845"
+  subscription_id = "722e98bb-a107-4d7c-8537-ba8c2f35547f"
   resource_provider_registrations = "none"
 }
